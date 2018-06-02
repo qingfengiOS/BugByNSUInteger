58b08696e7a23b013f47eb1b9f03ffbd9bbb9117
流年释残忆 <291753906@qq.com>
2018-06-02 23:27:11 +0800

    add

--- BugByNSUInteger/ViewController.m
+++ BugByNSUInteger/ViewController.m
@@ -16,14 +16,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSArray *array = @[];
+
+    NSUInteger uinteger = array.count;
+    NSLog(@"uinteger = %lu", uinteger);
+    
+    NSUInteger reduceUinteger = uinteger - 1;//由于是一个无符号整数，所以这里的返回的和预期值完全不同
+    NSLog(@"reduceUinteger = %lu", reduceUinteger);//18446744073709551615(它等于2的64次方-1,也就是64位系统的最大数减一)
+    
+    
+    NSInteger intger = (NSInteger)array.count;//这是强制转换NSUInteger为NSInteger
+    NSInteger reduceOfIntger = intger - 1;
+    NSLog(@"reduceOfIntger = %ld", reduceOfIntger);
 }
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 
 @end
